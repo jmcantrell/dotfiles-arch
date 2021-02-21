c329826932bd536893bbf5e144d3cc6bb558b567
Jeremy Cantrell <jmcantrell@gmail.com>
2021-02-21 14:19:28 -0600

    config changes

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,12 +5,7 @@ set -e
 # only if allowed to use sudo
 if groups | grep -wq wheel; then
 
-    sudo pacman -Sy --noconfirm --needed `cat depends/arch`
-
-    # gpg keys for cower
-    gpg --recv-keys --keyserver hkp://pgp.mit.edu 1EB2638FF56C0C53
-
     # use fallback installer to install aur helper
-    ./packages/arch/.local/bin/aur-install cower pacaur
+    ./packages/arch/.local/bin/aur-install yay
 
 fi
