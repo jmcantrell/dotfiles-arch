2d1cbdfbb9d73d768642556b28f69d62c50dbfcc
Jeremy Cantrell <jmcantrell@gmail.com>
2021-02-21 23:19:51 -0600

    config changes

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,7 +5,7 @@ set -e
 # only if allowed to use sudo
 if groups | grep -wq wheel; then
 
-    # use fallback installer to install aur helper
-    ./packages/arch/.local/bin/aur-install yay
+	# use fallback installer to install aur helper
+	./packages/arch/.local/bin/aur-install yay
 
 fi
