720f4f5113ff29344fa2afc3a7fd288646a48f99
Jeremy Cantrell <jmcantrell@gmail.com>
2021-05-22 04:11:56 -0500

    config changes

--- install.sh
+++ install.sh
@@ -13,19 +13,26 @@ pikaur -Sy --noconfirm --needed "${aur_packages[@]}"
 
 sudo rsync -a ./rootfs/ /
 
-sudo systemctl --now enable docker.service
+sudo systemctl enable --now mullvad-daemon.service
+mullvad account account set "$MULLVAD_ACCOUNT"
+mullvad auto-connect set on
+mullvad lan set allow
+mullvad relay set tunnel-protocol wireguard
+mullvad relay set location us nyc
+
+sudo systemctl enable --now docker.service
 sudo gpasswd -a "$USER" docker
 
 sudo ln -sfv /usr/share/fontconfig/conf.avail/10-sub-pixel-rgb.conf /etc/fonts/conf.d
 sudo ln -sfv /usr/share/fontconfig/conf.avail/11-lcdfilter-default.conf /etc/fonts/conf.d
 
-sudo systemctl --now enable bluetooth.service
+sudo systemctl enable --now bluetooth.service
 sudo gpasswd -a "$USER" lp
 
-sudo systemctl --now enable cups.socket
+sudo systemctl enable --now cups.socket
 
 sudo gpasswd -a "$USER" scanner
 sudo gpasswd -a "$USER" vboxusers
 
 ipfs init
-systemctl --user --now enable ipfs.service
+systemctl --user enable --now ipfs.service
