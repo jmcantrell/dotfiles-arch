80ea6076fa13d128e80fb15bd8893c78bc0c1121
Jeremy Cantrell <jmcantrell@gmail.com>
2021-07-01 23:44:05 -0500

    It probably makes more sense at this step

--- config.sh
+++ config.sh
@@ -6,3 +6,6 @@ sudo rsync ./rootfs/ /
 
 sudo ln -sfv /usr/share/fontconfig/conf.avail/10-sub-pixel-rgb.conf /etc/fonts/conf.d
 sudo ln -sfv /usr/share/fontconfig/conf.avail/11-lcdfilter-default.conf /etc/fonts/conf.d
+
+# Ensure that the /etc/skel files don't interfere.
+rm -f ~/.bashrc ~/.bash_profile ~/.bash_logout
--- stow.sh
+++ stow.sh
@@ -2,7 +2,4 @@
 
 set -euo pipefail
 
-# Ensure that the /etc/skel files don't interfere.
-rm -f ~/.bashrc ~/.bash_profile ~/.bash_logout
-
 stow ${VERBOSE:+--verbose} --no-folding -d . -t ~ -R stow
