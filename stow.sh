15b5dc0189841f3adf6ef79af78fa9d87df245c5
Jeremy Cantrell <jmcantrell@gmail.com>
2021-07-01 23:46:20 -0500

    Revert "It probably makes more sense at this step"
    This reverts commit 80ea6076fa13d128e80fb15bd8893c78bc0c1121.

--- stow.sh
+++ stow.sh
@@ -2,4 +2,7 @@
 
 set -euo pipefail
 
+# Ensure that the /etc/skel files don't interfere.
+rm -f ~/.bashrc ~/.bash_profile ~/.bash_logout
+
 stow ${VERBOSE:+--verbose} --no-folding -d . -t ~ -R stow
