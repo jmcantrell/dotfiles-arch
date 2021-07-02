10d2cad2b7abe53471933889c7f76a9c9d2ead00
Jeremy Cantrell <jmcantrell@gmail.com>
2021-07-01 23:21:44 -0500

    I forgot that -a copies ownership

--- config.sh
+++ config.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-sudo rsync -a ./rootfs/ /
+sudo rsync ./rootfs/ /
 
 sudo ln -sfv /usr/share/fontconfig/conf.avail/10-sub-pixel-rgb.conf /etc/fonts/conf.d
 sudo ln -sfv /usr/share/fontconfig/conf.avail/11-lcdfilter-default.conf /etc/fonts/conf.d
