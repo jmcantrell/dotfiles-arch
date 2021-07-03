36f997eb761668f8fe01d5fe8c56d4b5e52e89ec
Jeremy Cantrell <jmcantrell@gmail.com>
2021-07-03 09:52:01 -0500

    Unused

--- stow.sh
+++ stow.sh
@@ -5,4 +5,4 @@ set -euo pipefail
 # Ensure that the /etc/skel files don't interfere.
 rm -f ~/.bashrc ~/.bash_profile ~/.bash_logout
 
-stow ${VERBOSE:+--verbose} --no-folding -d . -t ~ -R stow
+stow --no-folding -d . -t ~ -R stow
