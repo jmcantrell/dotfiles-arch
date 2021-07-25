bd34f2af2f39be6391b2dc5093e7bb73975bcc00
Jeremy Cantrell <jmcantrell@gmail.com>
2021-07-25 04:34:21 -0500

    Pacman can handle stdin

--- install.sh
+++ install.sh
@@ -8,12 +8,10 @@ pacman_list=$PWD/packages/pacman${name:+-$name}
 aur_list=$PWD/packages/aur${name:+-$name}
 
 if [[ -f $pacman_list ]]; then
-    readarray -t pacman_packages <"$pacman_list"
-    sudo pacman -Sy --noconfirm --needed "${pacman_packages[@]}"
+    sudo cat "$pacman_list" | sudo pacman -Sy --noconfirm --needed -
 fi
 
 if [[ -f $aur_list ]]; then
     which pikaur &>/dev/null || ./stow/.local/bin/aur-install pikaur
-    readarray -t aur_packages <"$aur_list"
-    pikaur -Sy --noconfirm --needed "${aur_packages[@]}"
+    pikaur -Sy --noconfirm --needed - <"$aur_list"
 fi
