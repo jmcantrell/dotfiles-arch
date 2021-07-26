c7dfd166bb9e3e4b8e6bb965a81eb0d2e051f85e
Jeremy Cantrell <jmcantrell@gmail.com>
2021-07-26 14:19:45 -0500

    Don't update the package database

--- install.sh
+++ install.sh
@@ -8,10 +8,10 @@ pacman_list=$PWD/packages/pacman${name:+-$name}
 aur_list=$PWD/packages/aur${name:+-$name}
 
 if [[ -f $pacman_list ]]; then
-    sudo cat "$pacman_list" | sudo pacman -Sy --noconfirm --needed -
+    sudo cat "$pacman_list" | sudo pacman -S --noconfirm --needed -
 fi
 
 if [[ -f $aur_list ]]; then
     which pikaur &>/dev/null || ./stow/.local/bin/aur-install pikaur
-    pikaur -Sy --noconfirm --needed - <"$aur_list"
+    pikaur -S --noconfirm --needed - <"$aur_list"
 fi
