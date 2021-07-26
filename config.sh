089c25be6b63ea915f48e44ef3a3e6f8f576b621
Jeremy Cantrell <jmcantrell@gmail.com>
2021-07-26 14:18:57 -0500

    Ensure owner is root

--- config.sh
+++ config.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-sudo rsync -b --suffix=.pacnew ./rootfs/ /
+sudo rsync -avb --no-owner --no-group --suffix=.pacnew ./rootfs/ /
 
 sudo ln -sfv /usr/share/fontconfig/conf.avail/10-sub-pixel-rgb.conf /etc/fonts/conf.d
 sudo ln -sfv /usr/share/fontconfig/conf.avail/11-lcdfilter-default.conf /etc/fonts/conf.d
