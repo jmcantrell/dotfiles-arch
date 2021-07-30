f1d8253223b7481c1a568ef78d120cb75df2a535
Jeremy Cantrell <jmcantrell@gmail.com>
2021-07-30 04:15:12 -0500

    Only do certain stuff when x is installed

--- config.sh
+++ config.sh
@@ -4,5 +4,9 @@ set -euo pipefail
 
 sudo rsync -avb --no-owner --no-group --suffix=.pacnew ./rootfs/ /
 
-sudo ln -sfv /usr/share/fontconfig/conf.avail/10-sub-pixel-rgb.conf /etc/fonts/conf.d
-sudo ln -sfv /usr/share/fontconfig/conf.avail/11-lcdfilter-default.conf /etc/fonts/conf.d
+if which Xorg &>/dev/null; then
+    sudo ln -sfv /usr/share/fontconfig/conf.avail/10-sub-pixel-rgb.conf /etc/fonts/conf.d
+    sudo ln -sfv /usr/share/fontconfig/conf.avail/11-lcdfilter-default.conf /etc/fonts/conf.d
+
+    sudo systemctl enable xdg-user-dirs-update.service
+fi
