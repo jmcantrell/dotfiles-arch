3b43f951aeb41fd10b3e79fea39c312f3ab558f1
Jeremy Cantrell <jmcantrell@gmail.com>
2021-08-05 05:52:51 -0500

    Moved to linux configs

--- config.sh
+++ config.sh
@@ -4,19 +4,10 @@ set -euo pipefail
 
 sudo rsync -avb --no-owner --no-group --suffix=.pacnew ./rootfs/ /
 
-sudo ufw enable
-sudo ufw default deny
-sudo ufw allow ssh
-sudo ufw allow mdns
-sudo ufw allow llmnr
-
 sudo systemctl enable atd.service
 
 sudo systemctl enable pkgfile-update.timer
 sudo pkgfile -u
-
-sudo gpasswd -a "$USER" locate
-updatedb
 
 if which Xorg &>/dev/null; then
     sudo ln -sfv /usr/share/fontconfig/conf.avail/10-sub-pixel-rgb.conf /etc/fonts/conf.d
