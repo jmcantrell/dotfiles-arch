a79bd238ebea6fbff7613601cfa68ae5231ff02d
Jeremy Cantrell <jmcantrell@gmail.com>
2021-08-09 22:18:23 -0500

    Don't enable ssh-agent during stow

--- config.sh
+++ config.sh
@@ -9,6 +9,8 @@ sudo systemctl enable atd.service
 sudo systemctl enable pkgfile-update.timer
 sudo pkgfile -u
 
+systemctl --user enable --now ssh-agent.service
+
 if which Xorg &>/dev/null; then
     sudo ln -sfv /usr/share/fontconfig/conf.avail/10-sub-pixel-rgb.conf /etc/fonts/conf.d
     sudo ln -sfv /usr/share/fontconfig/conf.avail/11-lcdfilter-default.conf /etc/fonts/conf.d
